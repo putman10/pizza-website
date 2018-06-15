bb35964e4272c6f8e0b597fbf686bc5808d01533
Ryan Putman <putman10@me.com>
2018-06-15 10:08:18 -0700

    add tabs for toppings and topping images in img folder.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,15 @@
 // Business Logic
 
+function Pizza (userName) {
+  this.name = userName;
+  this.size = "";
+  this.toppings = [];
+}
 
-// Allow the user to choose toppings and size for the pizza they'd like to order.
-//
 
-
+Pizza.prototype.calculateCost = function() {
+  return (COST)
+}
 
 // Create a pizza object constructor with properties for toppings and size.
 //
@@ -15,3 +20,15 @@
 // Create a prototype method for the cost of a pizza depending on the selections chosen. Use your own formula for this.
 
 // User Interface Logic
+
+$(document).ready(function() {
+
+  $("#start-order").click(function() {
+    var userName = $("input#user-name").val();
+    $("#user-name-fields").hide();
+    $("span#user-name-span").text(userName);
+    $("#pizza-customizer").fadeIn(2000);
+
+    })
+
+});
