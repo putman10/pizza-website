f08bb44578c00c0bd98d947a92e205825514bb20
Ryan Putman <putman10@me.com>
2018-06-15 09:25:21 -0700

    setup inital html and css for visually thinking about functionality. Also sets up scripts with main functionality and sections for business/ui logic.

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+// Business Logic
+
+
+// Allow the user to choose toppings and size for the pizza they'd like to order.
+//
+
+
+
+// Create a pizza object constructor with properties for toppings and size.
+//
+
+
+
+
+// Create a prototype method for the cost of a pizza depending on the selections chosen. Use your own formula for this.
+
+// User Interface Logic
