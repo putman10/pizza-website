409d206cd7be37331547fe6dea40aea7c8a0844e
Ryan Putman <putman10@me.com>
2018-06-15 11:53:47 -0700

    add veggies and protein function to dynamitcally push if selected to empty array

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
 // Business Logic
-function Reciept (userName) {
+function Receipt (userName) {
   this.name = userName;
   // totalCost = pizza + pizza
 }
 
-function Pizza () {
+function Pizza (userName, proteins, veggies) {
   this.size = "";
   this.toppings = [];
   this.cost = "";
@@ -26,16 +26,33 @@ Pizza.prototype.calculateCost = function() {
 // User Interface Logic
 
 $(document).ready(function() {
-
+$("#pizza-customizer").fadeIn(2000);
   $("#start-order").click(function() {
     var userName = $("input#user-name").val();
-    var reciepts = new Reciept(userName);
+    var receipts = new Receipt(userName);
+    var proteins = [];
+    var veggies = [];
     $("#user-name-fields").hide();
     $("span#user-name-span").text(userName);
-    $("#pizza-customizer").fadeIn(2000);
+    // $("#pizza-customizer").fadeIn(2000);
+console.log(veggies);
 
     $(".col-md-2").click(function() {
+      var $this = $(this).attr('class');
+console.log($this);
       $(this).toggleClass("selected-topping");
+      $(this. img).toggleClass("select");
+      // var veggies = $(this).attr('id');
+
+      if ($this.is('.selected-topping')) {
+       veggies.push($(this).attr('id'));
+      } else {
+       veggies.splice[$(this).attr('id')];
+      }
+    });
+
+    $("#add-to-cart").click(function() {
+      var pizza = new Pizza(userName, proteins, veggies);
     });
   });
 
